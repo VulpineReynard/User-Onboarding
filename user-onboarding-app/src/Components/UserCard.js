72e8547ca80b86a669b8b0b0ea90425a0c4e4ff2
VulpineReynard <milare8@gmail.com>
2019-08-07 18:08:13 -0400

    more styling

--- user-onboarding-app/src/Components/UserCard.js
+++ user-onboarding-app/src/Components/UserCard.js
@@ -7,7 +7,8 @@ const UserCard = ({ user }) => {
 
         <div class="user-card-front">
           <h2>{user.name}</h2>
-          <p>Hover Over Me</p>
+          <p>id: {user.id}</p>
+          <p className="hover-text">Hover Over Me</p>
         </div>
 
         <div class="user-card-back">
