d8409b14f8c063ff8a8a45703167c1592b5f47a2
VulpineReynard <milare8@gmail.com>
2019-08-07 17:43:11 -0400

    MVP Complete

---
+++ user-onboarding-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import FormikForm from './Components/Form';
+import Header from './Components/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <FormikForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ user-onboarding-app/src/Components/UserCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const UserCard = ({ user }) => {
+  console.log(user);
+  return(
+    <div class="user-card">
+      <div class="user-card-inner">
+
+        <div class="user-card-front">
+          <h2>{user.name}</h2>
+          <p>Hover Over Me</p>
+        </div>
+
+        <div class="user-card-back">
+          <p className="email">Email</p>
+          <p>{user.email}</p> 
+          <p className="password">Password</p>
+          <p>{user.password}</p>
+        </div>
+        
+      </div> 
+    </div>
+  );
+}
+
+export default UserCard;
